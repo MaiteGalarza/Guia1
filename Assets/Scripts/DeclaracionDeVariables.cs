c7462ebbe60ff72ec4c163a6774eb36abc708df6
49189070 <49189070@est.ort.edu.ar>
2025-03-18 14:25:27 -0300

    Variables declaradas

---
+++ Assets/Scripts/DeclaracionDeVariables.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeclaracionDeVariables : MonoBehaviour
+{
+    // Start is called before the first frame update
+    int cantidadAlumnos;
+    float kilosVendidos;
+    string nombreAlumno;
+    bool estaAprobado;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
